7a2e029d6bcf440e94e0a671c87a123fdd26ea03
Mayur Patel <pmayur0680@gmail.com>
2022-05-21 22:06:52 -0500

    route to add thought

--- conrollers/thoughtController.js
+++ conrollers/thoughtController.js
@@ -8,7 +8,17 @@ module.exports = {
     // `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated 
     // user's `thoughts` array field)
     createThought(req, res) {
-        res.send('`POST` to create a new thought');
+        Thought.create( { thoughtText: req.body.thoughtText, username: req.body.username } )
+        .then (( {_id} ) => {
+            // also update user to push new thought in array
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: _id }}, 
+                { new: true, runValidators: true }
+            );
+        })
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
     },
     // `GET` to get a single thought by its `_id`
     getSingleThought(req, res) {
