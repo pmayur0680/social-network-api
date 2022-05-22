f68072435a980f35e5b1bc50d6f371343173be15
Mayur Patel <pmayur0680@gmail.com>
2022-05-21 20:23:03 -0500

    user route for update and delete

--- conrollers/userController.js
+++ conrollers/userController.js
@@ -1,5 +1,4 @@
-const { User } = require('../models');
-const ObjectId = require('mongodb').ObjectId;
+const { User, Thought } = require('../models');
 
 module.exports = {
     // `GET` all users - Done
@@ -17,9 +16,9 @@ module.exports = {
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
     },
-    // `GET` a single user by its `_id` and populated thought and friend data
+    // `GET` a single user by its `_id` and populated thought and friend data - Done
     getSingleUser(req, res) {        
-        User.findOne({ '_id': req.params.id })
+        User.findOne({ _id: req.params.id })
         .select('-__v')
         .populate('thoughts') // need to verify later
         .populate('friends') // need to verify later
@@ -30,14 +29,30 @@ module.exports = {
         )
         .catch((err) => res.status(500).json(err));
     },
-    //  `PUT` to update a user by its `_id`
+    //  `PUT` to update a user by its `_id` - Done
     updateSingleUser(req, res) {
-        res.send('`PUT` to update a user by its `_id`');
+        User.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+        .then((user) =>         
+        !user
+        ? res.status(400).json({ message: 'No user with that ID' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
     },
-    // `DELETE` to remove user by its `_id`
+    // `DELETE` to remove user by its `_id` - Done
     deleteUser(req, res) {
-        res.send('`DELETE` to remove user by its `_id`');
-
+        User.findOneAndDelete({ _id: req.params.id })
+          .then((user) =>         
+          !user
+          ? res.status(400).json({ message: 'No user with that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thought } })  // need to verify later
+          )
+          .then(() => res.json({ message: 'User and thoughts deleted!' }))
+          .catch((err) => res.status(500).json(err));
     },
      // `POST` to add a new friend to a user's friend list
      createUserFriend(req, res) {
